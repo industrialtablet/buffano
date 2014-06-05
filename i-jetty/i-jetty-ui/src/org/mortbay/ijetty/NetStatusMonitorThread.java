4dfcd8058698515e32b2b7b93b164274264945ff
smallstar <pengyixing@gmail.com>
2014-06-05 15:39:09 +0800

    code optimization

--- i-jetty/i-jetty-ui/src/org/mortbay/ijetty/NetStatusMonitorThread.java
+++ i-jetty/i-jetty-ui/src/org/mortbay/ijetty/NetStatusMonitorThread.java
@@ -8,7 +8,6 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 
